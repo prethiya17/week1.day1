8945daae36f8cac05ec608d0b916e09e6c29f136
prethiya17 <102045571+prethiya17@users.noreply.github.com>
2022-03-21 23:46:38 +0530

    Add files via upload

---
+++ PositiveOrNegativeNumber.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class PositiveOrNegativeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//	1. Initialize a number, say, int number= 35;
+		// 2. If a number is positive, print "The number is positive"
+		  //  If a number is negative, print "The number is negative"
+		  //  If it nether negative nor positive, print as "The number is neither positive nor negative"
+		
+		int num=0;
+				if(num>0) {
+					System.out.println("The Number is Positive");
+				}
+				else if(num<0) {
+					System.out.println("The Number is negative");
+				}
+				else {
+					System.out.println("The number is neither positive nor negative");
+				}
+	}
+
+}
